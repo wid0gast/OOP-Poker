e7d260395c2f67a1f15bf9bb16259a1df178aac9
Shaz Furniturewala <72245851+shortguyshaz@users.noreply.github.com>
2021-11-16 14:52:41 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -5,25 +5,34 @@ public class Player {
     String name;
     int currentChips;
     ArrayList<Card> holeCards = new ArrayList<>();
+    int playerBet = Table.currentBet;
 
     Player(String n, int i) {
         name = n;
         currentChips = i;
     }
 
-    void check() {
-
-    }
-
-    void raise(int n) {
-
-    }
-
     void call() {
+        if(currentChips < Table.currentBet) {
+            System.out.println("Going All In!");
+            playerBet = currentChips;
+        }
+        currentChips -= playerBet;
+    }
 
+    void check() {
+        
+    }
+
+    void raise(int n) throws NotEnoughChipsException {
+        if(currentChips < Table.currentBet) {
+            throw new NotEnoughChipsException("NOT ENOUGH CHIPS!")
+        }
+        currentChips -= playerBet;
+        Table.currentBet = playerBet; 
     }
 
     void fold() {
-        
+        Table.players.remove(this);
     }
 }
